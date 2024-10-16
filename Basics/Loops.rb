21e71b7a587b6887ce6786c147b93191f5d966c5
Damien Snow <d.snow@Damiens-MacBook-Air.local>
2024-10-16 20:22:31 +0100

    Finished adding content to all files

--- Basics/Loops.rb
+++ Basics/Loops.rb
@@ -1,2 +1,11 @@
 # frozen_string_literal: true
 
+3.times do    # times: Repeats a block of code a specific number of times.
+  puts "Hello!"
+end
+
+# Loop through and array
+fruits = ["apple", "banana", "pear"]
+fruits.each do |fruit|    # each: Iterates over each element of a collection like an array.
+  puts fruit
+end
--- Basics/MethodsAndFunctions.rb
+++ Basics/MethodsAndFunctions.rb
@@ -1,2 +1,8 @@
 # frozen_string_literal: true
 
+def greet(name)     # def: Defines a method.
+  puts "Heyya, #{name}!"
+end
+
+greet("Jack") # greet("Alice"): Calls the method with the argument "Alice".
+greet("Jill")
