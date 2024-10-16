c96ef0198d2a994229e53a4ac309715b731be9d7
Damien Snow <d.snow@Damiens-MacBook-Air.local>
2024-10-16 19:56:48 +0100

    Added the basic concepts folder

---
+++ Basics/FlowControl.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+age = 18
+
+if age >= 18    # if: Executes the block of code if the condition is true.
+  puts "You can get wasted in town."
+else            # else: Executes the block if the condition is false.
+  puts "You're probably getting wasted in the woods."
+end
---
+++ Basics/Variables.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+name = "Jack Frost"
+age = 25
+
+puts "My name is #{name} and I am #{age} years old"
